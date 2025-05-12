cfa47c822089d933e29bb998bbdb26f679fcdc56
Phan Van Lan <phanvanlan@gmail.com>
2025-05-12 22:51:15 +0700

    asdasd

--- ota_test.py
+++ ota_test.py
@@ -1,7 +1,7 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://github.com/phanvanlan/micropython-ota/main/"
+firmware_url = "https://github.com/phanvanlan/micropython-ota/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "ota_test.py")
 
