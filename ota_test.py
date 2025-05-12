9fe1703450954b1da89b274b5f638a393f77eeaf
Phan Van Lan <phanvanlan@gmail.com>
2025-05-12 22:50:21 +0700

    first commit

---
+++ ota_test.py
@@ -0,0 +1,8 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://github.com/phanvanlan/micropython-ota/main/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "ota_test.py")
+
+ota_updater.download_and_install_update_if_available()
