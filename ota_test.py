857e136a38da5f019a97c500053023cfe97c8ebe
Phan Van Lan <phanvanlan@gmail.com>
2025-05-13 00:29:56 +0700

    asdasdas

--- ota_test.py
+++ ota_test.py
@@ -5,10 +5,10 @@ from neopixel import NeoPixel
 from time import sleep
 
 firmware_url = "https://github.com/phanvanlan/micropython-ota/"
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "boot.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "ota_test.py")
 ota_updater.download_and_install_update_if_available()
 
 pin = Pin(8, Pin.OUT)  
 np = NeoPixel(pin, 1)  
-np[0] = (0, 0, 25)  
+np[0] = (25, 0, 0)  
 np.write()            
