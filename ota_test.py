2d1bd48639eb0901ab4970e24981b6dd469216b5
Phan Van Lan <phanvanlan@gmail.com>
2025-05-12 22:53:30 +0700

    asdasd

--- ota_test.py
+++ ota_test.py
@@ -6,3 +6,6 @@ firmware_url = "https://github.com/phanvanlan/micropython-ota/"
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "ota_test.py")
 
 ota_updater.download_and_install_update_if_available()
+
+
+print("heelo");
