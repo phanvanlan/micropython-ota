34bd5197407aee3927ecc35b2a2c7c483c351848
Phan Van Lan <phanvanlan@gmail.com>
2025-05-12 23:14:15 +0700

    asdasd

--- boot.py
+++ boot.py
@@ -1,11 +1,14 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep
 
 firmware_url = "https://github.com/phanvanlan/micropython-ota/"
-
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "boot.py")
-
 ota_updater.download_and_install_update_if_available()
 
-
-print("version 5");
+pin = Pin(8, Pin.OUT)  
+np = NeoPixel(pin, 1)  
+np[0] = (0, 25, 0)  
+np.write()            
